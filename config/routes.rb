ad7fcc6f37ead5de581f0b735c6ffeafc933d092
Adrian J <113844357+xAxJx@users.noreply.github.com>
2026-01-20 21:04:32 +0900

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  # root "lists#index"
+
+  root to: "lists#index"
 
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
